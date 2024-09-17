58cf3f1b90294ea94a9293e7742ead16e7ebfd4a
abner G Jacobsen <abner@apoana.com.br>
2024-09-17 12:19:36 -0300

    feat(user.py): add endpoint to schedule user creation 5 minutes in the future feat(user.py): add endpoint to refresh oauth2 credentials feat(user.py): add endpoint to retrieve all users fix(user.py): change single quotes to double quotes for consistency

--- app/controllers/http/v1/user.py
+++ app/controllers/http/v1/user.py
@@ -1,5 +1,7 @@
 from typing import Annotated
 
+import pendulum
+
 from dishka.entities.depends_marker import FromDishka as Depends
 from dishka.integrations.fastapi import inject
 from fastapi import APIRouter, Body, status
@@ -9,18 +11,28 @@ from app import models
 from app.controllers.http import deps
 from app.usecases import Services
 
-router = APIRouter(prefix='/user', tags=['user'])
+router = APIRouter(prefix="/user", tags=["user"])
 
 
-@router.post('/', response_class=Response, status_code=status.HTTP_201_CREATED)
+@router.post("/", response_class=Response, status_code=status.HTTP_201_CREATED)
 @inject
 async def user_create(data: models.UserCreate, service: Depends[Services]) -> None:
     """Create new user"""
-    await service.adapters.rabbit.create_task(data, 'create', exchange='user')
-    # await service.user.create(data)
+    await service.adapters.rabbit.create_task(data, "create", exchange="user")
 
 
-@router.post('/oauth2', response_model=models.UserAuth)
+@router.post("/schedule", response_class=Response, status_code=status.HTTP_201_CREATED)
+@inject
+async def user_create_later(
+    data: models.UserCreate, service: Depends[Services]
+) -> None:
+    """Create new user 5 minutes in the future"""
+    await service.adapters.rabbit.create_task(
+        data, "create", exchange="user", when=pendulum.now("UTC").add(minutes=5)
+    )
+
+
+@router.post("/oauth2", response_model=models.UserAuth)
 @inject
 async def user_oauth2(
     data: models.UserCreate,
@@ -31,7 +43,17 @@ async def user_oauth2(
     return await services.user.oauth2(user)
 
 
-@router.get('/', response_model=models.UserBase)
+@router.post("/oauth2/refresh", response_model=models.UserAuth)
+@inject
+async def user_oauth2_refresh(
+    refresh_token: Annotated[str, Body(embed=True)],
+    services: Depends[Services],
+) -> models.UserAuth:
+    """Refresh oauth2 credentials"""
+    return await services.user.refresh_oauth2(refresh_token)
+
+
+@router.get("/", response_model=models.UserBase)
 @inject
 async def user_retrieve(
     user: deps.CurrentUser,
@@ -40,11 +62,12 @@ async def user_retrieve(
     return models.UserBase.model_validate(user)
 
 
-@router.post('/oauth2/refresh', response_model=models.UserAuth)
+@router.get("/all", response_model=list[models.UserBase])
 @inject
-async def user_oauth2_refresh(
-    refresh_token: Annotated[str, Body(embed=True)],
+async def user_retrieve_all(
+    user: deps.CurrentUser,
     services: Depends[Services],
-) -> models.UserAuth:
-    """Refresh oauth2 credentials"""
-    return await services.user.refresh_oauth2(refresh_token)
+) -> list[models.UserBase]:
+    """Get all users"""
+    users = await services.user.retrieve_all()
+    return [models.UserBase.model_validate(u) for u in users]
